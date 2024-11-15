899d22c26254b36d6fe1edfdb677fbeb17ca0c86
Nicholas Duffew <pbsvimqx@students.codeinstitute.net>
2024-11-15 09:35:38 +0000

    feat: write the game object using TDD

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: []
+}
+
+// export the code to test file as { game }
+module.exports = { game };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,11 +2,38 @@
  * @jest-environment jsdom
  */
 
-// load index.html into jest's mock DOM before anything else
+// define { game } as a constant from game.js
+const { game } = require("../game");
+
+/** 
+ * load index.html into jest's mock DOM before anything else runs.
+ * This code will be the same for every html file we want to load into the mock DOM 
+ * just change the file name
+*/
 beforeAll(() => {
+    // install fs library
     let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "uft-8");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
     document.open();
     document.write(fileContents);
     document.close();
-})
+});
+
+// create a failing test to check that the score key exists
+describe("game object contains the correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    // next create a failing test to check if the currentGame key exists
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    // next create a failing test to check if the playerMoves key exists
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    // next create a failing test to check if the choices key exists
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+});
